e536f7fa0c29b98ea2293c42bba2293d2b3f6a3d
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2023-12-08 18:48:19 -0300

    alterações especificas nas genericViews

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 
@@ -17,8 +20,25 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
 class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+    print(queryset[0].curso)
+
+    def get_queryset(self):
+        lista = []
+        if self.kwargs.get('curso_pk'):
+            for q in self.queryset.all():
+                if q.curso.id == self.kwargs.get('curso_pk'):
+                    lista.append(q)
+            if len(lista) > 0:
+                return lista
+            # return self.queryset.filter(curso_id == self.kwargs.get('curso_pk'))
+        return self.queryset.all()
 
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get['curso_pk'], pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
